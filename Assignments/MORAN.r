8570ced274f1aec0202cc21d775592e9814ae584
benneaux <benjamin.g.moran@gmail.com>
2016-08-26 16:49:18 +1000

    finished

--- Assignments/MORAN.r
+++ Assignments/MORAN.r
@@ -2,69 +2,83 @@
 # ID No.: c3076448
 # STAT3040 - R Assignment 2
 
-require(astsa)
+# Setup =====================================================
 
-##1. How many data are stored in the variable jj?
+  require(astsa)
 
-  len.jj <- length(jj)
-  print(
-    paste(
-      "The no. of datum stored in the Johnson and Johnson Dataset:",
-      len.jj
-      )
-    )
+#  Q1. ======================================================
 
-
-##2. What are the average and variance of the Johnson & Johnson quarterly
-#    earnings per share from 1960 to 1984?
-
-  mean(jj) # The mean of JJ's quarterly earings from 1960:1984
-  var(jj)  # The variance of the same earnings
-
-
-##3. Plot the time series of the Johnson & Johnson quarterly earnings per share 
-#    from 1960 to 1984. The labels of the horizontal and vertical axes should be 
-#    in bold-style with the titles ‘Quarterly Earnings per Share’ and ‘Time’, 
-#    respectively.
+  # How many data are stored in the variable jj?
   
-  plot.ts(
-    jj,
-    ylab=expression(
-      bold("Quarterly Earnings per Share")),
-    xlab=expression(
-          bold("Time")),
-    main="Johnson & Johnson Quarterly Earnings per Share: \n 1960 - 1984"
-    )
+    len.jj <- length(jj) #No. of datum in jj
+    print(
+      paste(
+        "The no. of datum stored in the Johnson and Johnson Dataset:",
+        len.jj
+        )
+      )
+    
 
-#Note: if you have ggplot2, ggfortify and ggthemes installed, 
-#      run the code below: It will produce a much nicer plot.
-# require(ggplot2)
-# require(ggfortify)
-# require(ggthemes)
-# 
-# ggplot(fortify(jj), aes(Index,Data, colour = cycle(jj))) +
-#   geom_line(size=1.5) +
-#   labs(
-#     list(
-#       title="Johnson & Johnson Quarterly Earnings per Share: \n 1960 - 1984",
-#       x=expression(bold("Quarterly Earnings per Share")),
-#       y=expression(bold("Time"))
-#       )) +
-#   scale_colour_gradient2_tableau() +
-#   guides(colour= guide_legend("Quarter Change")) +
-#   theme_light()
+#  Q2. ======================================================
 
-##4. Plot the autocovariance function of this time series within 16 lags.
-#    The main title of this plot should be ‘Johnson & Johnson Quarterly 
-#    Earnings per Share’.
+  #  What are the average and variance of the Johnson & Johnson quarterly
+  #  earnings per share from 1960 to 1984?
+  
+    mean(jj) # The mean of JJ's quarterly earings from 1960:1984
+    var(jj)  # The variance of the same earnings
+    
 
-  acf(
-    jj,
-    16,
-    type=c("covariance"),
-    main="Johnson & Johnson Quarterly Earnings per Share"
-    )
+#  Q3. ======================================================
+  
+  #  Plot the time series of the Johnson & Johnson quarterly earnings per share 
+  #  from 1960 to 1984. The labels of the horizontal and vertical axes should be 
+  #  in bold-style with the titles ‘Quarterly Earnings per Share’ and ‘Time’, 
+  #  respectively.
+    
+    plot.ts(
+      jj,
+      ylab=expression(
+        bold("Quarterly Earnings per Share")),
+      xlab=expression(
+            bold("Time")),
+      main="Johnson & Johnson Quarterly Earnings per Share:\n 1960 - 1984"
+      )
+    
 
-## References
+#  Q3. Extra ================================================
+  
+  #Note: if you have ggplot2, ggfortify and ggthemes installed, 
+  #      run the code below: It will produce a much nicer plot.
+  # require(ggplot2)
+  # require(ggfortify)
+  # require(ggthemes)
+  # 
+  # ggplot(fortify(jj), aes(Index,Data, colour = cycle(jj))) +
+  #   geom_line(size=1.5) +
+  #   labs(
+  #     list(
+  #       title="Johnson & Johnson Quarterly Earnings per Share:\n 1960 - 1984",
+  #       x=expression(bold("Quarterly Earnings per Share")),
+  #       y=expression(bold("Time"))
+  #       )) +
+  #   scale_colour_gradient2_tableau() +
+  #   guides(colour= guide_legend("Quarter Change")) +
+  #   theme_light()
+    
+
+#  Q4. ======================================================
+
+  #  Plot the autocovariance function of this time series within 16 lags.
+  #  The main title of this plot should be ‘Johnson & Johnson Quarterly 
+  #  Earnings per Share’.
+  
+    acf(
+      jj,
+      16,
+      type=c("covariance"),
+      main="Johnson & Johnson Quarterly Earnings per Share:\n ACF"
+      )
+
+#  References ===============================================
 #  R.H. Shumway and D.S. Stoffer, Time Series Analysis and Its Applications With R Examples,
 #  Springer, New York, 2010.
